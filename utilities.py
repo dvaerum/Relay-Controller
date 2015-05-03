50e04aea11eea61c8e8b38a7cb106a9701c670b8
Dennis Vestergaard Værum <dennis.vaerum@gmail.com>
2015-05-03 13:32:44 +0200

    Added: my_print function Note: Is meant to easy reduce output by only printing then the 1. argument is 'debug'.

---
+++ utilities.py
@@ -0,0 +1,23 @@
+import sys
+
+__author__ = 'alt_mulig'
+
+
+debug = False
+if len(sys.argv) > 1 and sys.argv[1] == "debug":
+    debug = True
+
+
+def my_print(*args, sep=' ', end='\n', file=None):
+    if debug:
+        print(*args, sep=sep, end=end, file=file)
+
+
+def main():
+    global debug
+    debug = True
+    my_print("tester", "1212", end='\n\n')
+
+
+if __name__ == '__main__':
+    main()
