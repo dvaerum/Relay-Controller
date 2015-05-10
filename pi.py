2a206612509697718cba4b143a1995f4e22d6e17
Dennis Vestergaard Værum <dennis.vaerum@gmail.com>
2015-05-10 13:53:50 +0200

    Added: __fail_safe method Update: __pass_watt() method to work with __fail_safe Update: removed old uncommented code

--- pi.py
+++ pi.py
@@ -1,8 +1,10 @@
 from collections import deque
 from threading import Thread
+from threading import Timer
 import time
 from state_machine import StateMachine, RelayState
 import sys
+
 try:
     import RPIO as GPIO
 except SystemError:
@@ -17,13 +19,12 @@ class Watt:
 
     def add_pulse(self, gpio_id, value):
         # TODO: How to handle float overflow (time.perf_counter)
-        # Mads (vismanden) siger at det aldrig sker
+        # TODO: Mads (vismanden) siger at det aldrig sker
         self.__pulse.append(time.perf_counter())
+
         seconds = self.interval()
         if seconds >= 60:
-            # 1st try
-            # self.__kW = (len(self.__pulse) * 36)/seconds
-            self.__kW = (len(self.__pulse)/seconds)/0.36
+            self.__kW = (len(self.__pulse) / seconds) / 0.36
 
             if self.__event_function:
                 self.__event_function(self.__kW, seconds)
@@ -35,20 +36,13 @@ class Watt:
             sys.stdout.flush()
 
     def interval(self):
-        # return self.__pulse[len(self.__pulse)-1]-self.__pulse[0]
-        return self.__pulse[-1]-self.__pulse[0]
+        return self.__pulse[-1] - self.__pulse[0]
 
     def set_event_function(self, call_function):
         self.__event_function = call_function
 
 
 class PI(Thread):
-    """
-    class Relay(object):
-        __slots__ = ["watt", "switch_on", "switch_off", "GPIO", "number",
-                     "timestamp_switch_on", "timestamp_switch_off"]
-    """
-
     __watt = Watt()
     __state_machine = StateMachine()
 
@@ -56,6 +50,7 @@ class PI(Thread):
     __relays_count = 0
 
     __thread_relay = None
+    __timer_fail_safe = None
 
     def __init__(self, pin_interrupts):
         Thread.__init__(self)
@@ -63,10 +58,8 @@ class PI(Thread):
 
         GPIO.add_interrupt_callback(pin_interrupts, self.__watt.add_pulse, edge='rising')
         self.__watt.set_event_function(self.__pass_watt)
-        #self.__watt.set_event_function(self.print_info)
 
     def run(self):
-        self.__state_machine.start()
         GPIO.wait_for_interrupts()
 
     def stop(self):
@@ -85,13 +78,25 @@ class PI(Thread):
                            r.relay_number))
 
     def __pass_watt(self, kilo_watt, interval_time):
+        # TODO: Make it only print on debug
         print("%.4f kW (%.2fs)" % (kilo_watt, interval_time))
         sys.stdout.flush()
 
-
         if self.__state_machine.is_started():
-            if not self.__thread_relay or not self.__thread_relay.isAlive():
-                self.__thread_relay = Thread(target=self.__state_machine.next,
-                                            name="Thread StateMachine.next",
-                                            args=(kilo_watt, interval_time))
-                self.__thread_relay.start()
+            if not self.__thread_relay or not self.__thread_relay.is_alive():
+                if not self.__timer_fail_safe or not self.__timer_fail_safe.is_alive():
+                    self.__thread_relay = Thread(target=self.__state_machine.next,
+                                                 name="Thread StateMachine.next",
+                                                 args=(kilo_watt, interval_time))
+                    self.__thread_relay.start()
+        else:
+            self.__state_machine.start()
+
+        if self.__timer_fail_safe:
+            self.__timer_fail_safe.cancel()
+            # TODO: Add time value to the config file
+        self.__timer_fail_safe = Timer(60.5, self.__fail_safe)
+
+    def __fail_safe(self):
+        if not self.__thread_relay or not self.__thread_relay.is_alive():
+            self.__state_machine.stop()
