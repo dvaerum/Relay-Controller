830c32e91b13a570f248e7be04b21274821ebadb
Muhaha from Like A Boss <nn@nn.nn>
2015-08-28 19:21:23 +0200

    Added the option to use function and method instate of only classes containing a method named update.

--- lib/observable.py
+++ lib/observable.py
@@ -1,23 +1,36 @@
+import inspect
+
 __author__ = 'alt_mulig'
 __doc__ = "http://www.giantflyingsaucer.com/blog/?p=5117"
 
-class Observable(object):
 
+class Observable(object):
     def __init__(self):
-        self.observers = []
+        self.__observers = []
 
     def register(self, observer):
-        if observer not in self.observers:
-            self.observers.append(observer)
+        if observer not in self.__observers:
+            # TODO: test if type None is accepted
+            # TODO: test if the attr update is accepted if it is variable like "update = 4"
+            if (hasattr(observer, "update") and inspect.ismethod(observer.update)) or inspect.ismethod(observer) or inspect.isfunction(observer):
+                self.__observers.append(observer)
+            else:
+                raise TypeError("This is not a class-object, method or function")
 
     def unregister(self, observer):
-        if observer in self.observers:
-            self.observers.remove(observer)
+        if observer in self.__observers:
+            self.__observers.remove(observer)
 
     def unregister_all(self):
-        if self.observers:
-            del self.observers[:]
+        if self.__observers:
+            del self.__observers[:]
 
     def update_observers(self, *args, **kwargs):
-        for observer in self.observers:
-            observer.update(*args, **kwargs)
+        for observer in self.__observers:
+            if hasattr(observer, "update"):
+                observer.update(*args, **kwargs)
+            else:
+                observer(*args, **kwargs)
+
+    def count(self):
+        return len(self.__observers)
