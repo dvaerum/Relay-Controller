74070a103e684bb706e5466727eb28ffe24914ef
Muhaha from Like A Boss <nn@nn.nn>
2015-08-28 19:21:55 +0200

    Updated Unittest for observer

--- test/TestObserve.py
+++ test/TestObserve.py
@@ -7,6 +7,14 @@ from lib.observable import Observable
 from lib.observer import Observer
 
 
+def an_function(*args, **kwargs):
+    return
+
+
+class AnClass:
+    update = 3
+
+
 class AnObserver(Observer):
     def __init__(self):
         self.args = []
@@ -35,7 +43,7 @@ class observerable_test(unittest.TestCase):
         self.observable.unregister_all()
 
         # Should be zero registered
-        self.assertEqual(len(self.observable.observers), 0)
+        self.assertEqual(self.observable.count(), 0)
 
     def test_register(self):
         self.observable.register(self.observer1)
@@ -43,7 +51,7 @@ class observerable_test(unittest.TestCase):
         self.observable.register(self.observer3)
 
         # Should be three registered
-        self.assertEqual(len(self.observable.observers), 3)
+        self.assertEqual(self.observable.count(), 3)
         self.observable.unregister_all()
 
     def test_unregister(self):
@@ -55,7 +63,7 @@ class observerable_test(unittest.TestCase):
         self.observable.unregister(self.observer3)
 
         # Should be one registered
-        self.assertEqual(len(self.observable.observers), 1)
+        self.assertEqual(self.observable.count(), 1)
         self.observable.unregister_all()
 
     def test_update_observers(self):
@@ -65,6 +73,30 @@ class observerable_test(unittest.TestCase):
         self.assertEqual(self.observer1.args[0], 'abc')
         self.assertEqual(self.observer1.kwargs['msg'], '123')
         self.observable.unregister_all()
+
+    def test_type_handler(self):
+        self.assertIsNone(self.observable.register(self.observer1))
+        self.assertIsNone(self.observable.register(self.observer1.update))
+        self.assertIsNone(self.observable.register(AnObserver()))
+
+        error_msg = 'This is not a class-object, method or function'
+
+        with self.assertRaisesRegex(TypeError, error_msg):
+            self.observable.register(self.observer1.update())
+        with self.assertRaisesRegex(TypeError, error_msg):
+            self.observable.register(an_function())
+        with self.assertRaisesRegex(TypeError, error_msg):
+            self.observable.register(None)
+
+        with self.assertRaisesRegex(TypeError, error_msg):
+            self.observable.register(AnClass())
+        with self.assertRaisesRegex(TypeError, error_msg):
+            self.observable.register(AnClass)
+        with self.assertRaisesRegex(TypeError, error_msg):
+            self.observable.register(AnObserver)
+
+        with self.assertRaisesRegex(TypeError, error_msg):
+            self.observable.register(AnClass.update)
 
 
 if __name__ == "__main__":
